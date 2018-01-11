bfe05c7a5c428c1767756bf31012136694c564bf
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2018-01-11 18:22:10 +1300

    Added more buttons

--- src/main/SidePanel.java
+++ src/main/SidePanel.java
@@ -14,17 +14,23 @@ public class SidePanel {
             H = Main.HEIGHT - EDGE_SEP * 2;
     private TextButton ldMapBtn; // The load map button.
     private TextButton svMapBtn; // The save map button.
+    private TextButton ldTileArrBtn; // A button to load a tile arrangement.
+    private TextButton svTileArrBtn; // A button to save the current tile arrangement.
 
     public SidePanel() {
         ldMapBtn = new TextButton(X + 15, Y + 10, 150, 30, "Load Map");
         ldMapBtn.setTxtOffsetX(25);
         svMapBtn = new TextButton(X + 15, Y + 50, 150, 30, "Save Map");
         svMapBtn.setTxtOffsetX(25);
+        ldTileArrBtn = new TextButton(X + 15, Y + 90, 150, 30, "Load Tile Arr");
+        svTileArrBtn = new TextButton(X + 15, Y + 130, 150, 30, "Save Tile Arr");
     }
 
     public void update() {
         ldMapBtn.update();
         svMapBtn.update();
+        ldTileArrBtn.update();
+        svTileArrBtn.update();
     }
 
     public void draw(Graphics g) {
@@ -32,6 +38,8 @@ public class SidePanel {
         g.fillRoundRect(X, Y, W, H, EDGE_ARC, EDGE_ARC); // Draws the panel.
         ldMapBtn.draw(g);
         svMapBtn.draw(g);
+        ldTileArrBtn.draw(g);
+        svTileArrBtn.draw(g);
     }
 
 }
